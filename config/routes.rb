c024ccc16858db26fad698c04d555661210f57af
jalonsoad <juliocesar.alonso@gmail.com>
2020-11-15 15:51:20 +0100

    new

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,4 @@ scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
   	get "/acerca-de-juan",:to => "home#juan", :as => "juan"
  
   end
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
