cf34365baede3a6b93b5a602d0ff76aa5b8c7eff
jalonsoad <juliocesar.alonso@gmail.com>
2020-11-15 18:46:10 +0100

    new order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
   	@vote = Vote.new
-  	@allvotes = Vote.all
+  	@allvotes = Vote.all.order('created_at DESC')
   	@lastfivevotes = Vote.last(5)
   end
 end
